211cc82118a11287c26259be9d70d4a8b1d4dcc0
Christian Haselgrove <christian.haselgrove@umassmed.edu>
2019-03-05 11:01:02 -0500

    added OAuthCredentials.refresh() and OAuthCredentials.revoke()

--- h_annot/oauth.py
+++ h_annot/oauth.py
@@ -27,12 +27,41 @@ class OAuthClient(object):
 
 class OAuthCredentials(object):
 
-    def __init__(self, access_token, token_type, scope, expires, refresh_token):
-        self.access_token = access_token
-        self.token_type = token_type
-        self.scope = scope
-        self.expires = expires
-        self.refresh_token = refresh_token
+    def __init__(self, *args):
+        if len(args) == 1:
+            self._load_from_json(args[0])
+        else:
+            (access_token, token_type, scope, expires, refresh_token) = args
+            self.access_token = access_token
+            self.token_type = token_type
+            self.scope = scope
+            self.expires = expires
+            self.refresh_token = refresh_token
+        self.revoked = False
+        return
+
+    def _load_from_json(self, data):
+        obj = json.loads(data)
+        self.access_token = obj['access_token']
+        dt = datetime.timedelta(seconds=obj['expires_in'])
+        self.token_type = obj['token_type']
+        self.scope = obj['scope']
+        self.expires = datetime.datetime.now() + dt
+        self.refresh_token = obj['refresh_token']
+        return
+
+    def refresh(self):
+        self._load_from_json(api.refresh_oauth_token(self.refresh_token))
+        return
+
+    def revoke(self):
+        api.revoke_oauth(self.access_token)
+        self.access_token = None
+        self.token_type = None
+        self.scope = None
+        self.expires = None
+        self.refresh_token = None
+        self.revoked = True
         return
 
 # eof
--- tests/oauth.py
+++ tests/oauth.py
@@ -5,7 +5,7 @@ import unittest
 import h_annot.oauth
 from . import config, utils
 
-class TestOAuth(unittest.TestCase):
+class TestOAuthBasics(unittest.TestCase):
 
     def test_url(self):
         url = h_annot.api.oauth_url('client12345')
@@ -24,17 +24,42 @@ class TestOAuth(unittest.TestCase):
         self.assertEqual(client.auth_url(), 'https://hypothes.is/oauth/authorize?response_type=code&client_id=client12345')
         return
 
+class TestOAuth(unittest.TestCase):
+
     @config.server_test
     @config.oauth_test
-    def test_oauth_credentials(self, 
-                               client_id, 
-                               client_secret, 
-                               username, 
-                               password):
-        client = h_annot.oauth.OAuthClient(client_id, client_secret)
-        code = utils.get_oauth_code(client_id, username, password)
-        creds = client.get_credentials(code)
-        self.assertIsInstance(creds, h_annot.oauth.OAuthCredentials)
+    def setUp(self, client_id, client_secret, username, password):
+        self.client = h_annot.oauth.OAuthClient(client_id, client_secret)
+        self.code = utils.get_oauth_code(client_id, username, password)
+        self.creds = self.client.get_credentials(self.code)
+        return
+
+    @config.server_test
+    def test_oauth_credentials(self):
+        self.assertIsInstance(self.creds, h_annot.oauth.OAuthCredentials)
+        return
+
+    @config.server_test
+    def test_refresh_credentials(self):
+        access_token_0 = self.creds.access_token
+        expires_0 = self.creds.expires
+        refresh_token_0 = self.creds.refresh_token
+        self.creds.refresh()
+        self.assertNotEqual(self.creds.access_token, access_token_0)
+        self.assertNotEqual(self.creds.refresh_token, refresh_token_0)
+        self.assertGreater(self.creds.expires, expires_0)
+        return
+
+    @config.server_test
+    def test_revoke_credentials(self):
+        self.assertFalse(self.creds.revoked)
+        self.creds.revoke()
+        self.assertTrue(self.creds.revoked)
+        self.assertIsNone(self.creds.access_token)
+        self.assertIsNone(self.creds.token_type)
+        self.assertIsNone(self.creds.scope)
+        self.assertIsNone(self.creds.expires)
+        self.assertIsNone(self.creds.refresh_token)
         return
 
 # eof
