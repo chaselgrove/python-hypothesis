0cbbbb1b25e7f2ba205020a3a6b625ee1bb8df9b
Christian Haselgrove <christian.haselgrove@umassmed.edu>
2020-06-09 13:45:05 -0400

    Update CSRF token handling in OAuth tests

--- tests/utils.py
+++ tests/utils.py
@@ -5,6 +5,7 @@ try:
     import urlparse
 except ImportError:
     import urllib.parse as urlparse
+import re
 import requests
 import h_annot
 
@@ -14,11 +15,13 @@ import h_annot
 # at the time of writing and is therefore fragile to changes in the
 # Hypothesis server.
 def get_oauth_code(client_id, username, password):
+    csrf_re = re.compile('<input.*name="csrf_token" value="([^"]*)"')
     url = h_annot.api.oauth_url(client_id)
     with requests.Session() as s:
         response = s.get(url)
+        csrf_token = csrf_re.search(response.text).groups()[0]
         data = {'__formid__': (None, 'deform'),
-                'csrf_token': (None, response.cookies['XSRF-TOKEN']),
+                'csrf_token': (None, csrf_token), 
                 'username': (None, username),
                 'password': (None, password),
                 'Log_in': (None, 'Log_in')}
